09fc67624815ed0f455de9d5267c0508effded89
Mathluiz23 <matheus_luizs@hotmail.com>
2025-01-15 22:24:25 -0300

    :recycle: refactor: adjusting initial header config

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,26 +1,26 @@
 // src/app/components/Header.tsx
 import Link from 'next/link';
-import { motion } from 'framer-motion';
+// import { motion } from 'framer-motion';
 
 export default function Header() {
   return (
     <header className="header">
-      <motion.img
+      {/* <motion.img
         src="/logo.png"
         alt="Logo da Empresa"
         className="logo"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 1 }}
-      />
+      /> */}
       <nav className="nav">
-        <Link href="#sobre">
+        <Link href="#sobre" legacyBehavior>
           <a className="nav-link">Sobre</a>
         </Link>
-        <Link href="#localizacao">
+        <Link href="#localizacao" legacyBehavior>
           <a className="nav-link">Localização</a>
         </Link>
-        <Link href="#contato">
+        <Link href="#contato" legacyBehavior>
           <a className="nav-link">Contato</a>
         </Link>
       </nav>
