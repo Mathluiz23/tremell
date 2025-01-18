6601f883650dbf30dbb91e4423dcfd6df06d662d
Mathluiz23 <matheus_luizs@hotmail.com>
2025-01-18 13:36:12 -0300

    :recycle: refactor: adjusting header to render logo app

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,18 +1,25 @@
-// src/app/components/Header.tsx
 import Link from 'next/link';
+import Image from 'next/image';
 // import { motion } from 'framer-motion';
+import tremell from "./tremell.png";
 
 export default function Header() {
   return (
     <header className="header">
-      {/* <motion.img
-        src="/logo.png"
-        alt="Logo da Empresa"
-        className="logo"
+      {/* <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 1 }}
-      /> */}
+        // className="logo-container"
+      > */}
+        <Image
+          src={tremell}
+          alt="Logo da Empresa"
+          className="logo"
+          width={100}
+          height={100}
+        />
+      {/* </motion.div> */}
       <nav className="nav">
         <Link href="#sobre" legacyBehavior>
           <a className="nav-link">Sobre</a>
