572a08459787a72fcd360149e87776da22ea3473
Mathluiz23 <matheus_luizs@hotmail.com>
2025-01-19 20:52:28 -0300

    :wrench: config: enabling appDir and strict mode in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true, // Habilita o modo estrito do React
+  experimental: {
+    appDir: true, // Ativa o suporte para a estrutura com o diretório `src/app`
+  },
 };
 
 export default nextConfig;
