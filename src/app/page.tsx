fbb53229ac3091097b551d0ff4c012759c6124d9
Mathluiz23 <matheus_luizs@hotmail.com>
2025-01-19 21:12:31 -0300

    :recycle: refactor: adjusting path to import all components to render at main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-import Header from "./components/Header";
+// src/app/page.tsx
+// import Hero from './components/Hero';
+import Sobre from './components/Sobre';
+import Servicos from './components/Servicos';
+import Galeria from './components/Galeria';
+import Contato from './components/Contato';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 export default function Home() {
   return (
-    <div>
-        <Header />
-    </div>
+    <>
+      <Header/>
+      <Sobre />
+      <Servicos />
+      <Galeria />
+      <Contato />
+      <Footer />
+    </>
   );
 }
