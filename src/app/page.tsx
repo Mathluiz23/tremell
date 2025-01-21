b7e294b344394ad0c987b4b74a8b3d1da9335472
Mathluiz23 <matheus_luizs@hotmail.com>
2025-01-20 21:34:33 -0300

    :recycle: refactor: reconfig main page to fix css import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-// src/app/page.tsx
-// import Hero from './components/Hero';
+import Header from './components/Header';
 import Sobre from './components/Sobre';
 import Servicos from './components/Servicos';
 import Galeria from './components/Galeria';
 import Contato from './components/Contato';
 import Footer from './components/Footer';
-import Header from './components/Header';
 
 export default function Home() {
   return (
