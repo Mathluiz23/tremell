54ba906e9466f361023cdd3fbf117c52e816b6bd
Mathluiz23 <matheus_luizs@hotmail.com>
2025-01-20 22:48:37 -0300

    :recycle: refactor: adjusting components at main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
-import Header from './components/Header';
 import Sobre from './components/Sobre';
 import Servicos from './components/Servicos';
 import Galeria from './components/Galeria';
 import Contato from './components/Contato';
 import Footer from './components/Footer';
+import Main from './components/Main';
 
 export default function Home() {
   return (
     <>
-      <Header/>
+      <Main />
       <Sobre />
       <Servicos />
       <Galeria />
