493bb69c64008f0c9258b11fa18c9463d2be4117
Mathluiz23 <matheus_luizs@hotmail.com>
2025-01-21 21:54:55 -0300

    :recycle: refactor: adjusting about section to describe about the company

--- src/app/components/Sobre.tsx
+++ src/app/components/Sobre.tsx
@@ -3,10 +3,15 @@ import styles from '../styles/Sobre.module.css';
 export default function Sobre() {
   return (
     <section id="sobre" className={styles.sobre}>
-      <h2>Sobre Nós</h2>
-      <p>
-        Somos uma empresa especializada em terraplanagem, oferecendo serviços de qualidade para projetos residenciais, comerciais e industriais.
-      </p>
+      <div className={styles.sobreContent} >
+        <h2>Quem nós somos</h2>
+        <h3>Comprometidos com a satisfação do cliente</h3>
+        <p>
+          A Emell Terraplenagem é especializada em serviços de terraplenagem.
+          Nosso compromisso com a qualidade e o profissionalismo garante que atendemos às necessidades de construção e movimentação de terra de nossos clientes.
+          Priorizamos a satisfação do cliente, tornando-nos uma escolha de confiança no Rio Grande do Sul.
+        </p>
+      </div>
     </section>
   );
 }
