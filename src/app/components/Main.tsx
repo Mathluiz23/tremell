789a7047fddd380c8d4a4987afbf72fad2642540
Mathluiz23 <matheus_luizs@hotmail.com>
2025-01-23 23:08:30 -0300

    :recycle: refactor: adjusting main component to do effect when render page

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { motion } from 'framer-motion';
 import wallpaper from '../assets/wallpaper.png';
 import styles from '../styles/Main.module.css';
 import tremell from '../assets/tremell.png';
@@ -11,30 +14,44 @@ export default function Main() {
       style={{ backgroundImage: `url(${wallpaper.src})` }}
     >
       <header className={styles.header}>
-        <Image
-          src={tremell}
-          alt="Logo da Empresa"
-          className={styles.logo}
-          width={120}
-          height={120}
-        />
+        <motion.div
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
+          <Image
+            src={tremell}
+            alt="Logo da Empresa"
+            className={styles.logo}
+            width={120}
+            height={120}
+          />
+        </motion.div>
         <nav className={styles.nav}>
-          <Link href="#sobre" legacyBehavior>
-            <a className={styles.navLink}>Sobre</a>
-          </Link>
-          <Link href="#localizacao" legacyBehavior>
-            <a className={styles.navLink}>Localização</a>
-          </Link>
-          <Link href="#contato" legacyBehavior>
-            <a className={styles.navLink}>Contato</a>
-          </Link>
+          {['#sobre', '#localizacao', '#contato'].map((href, index) => (
+            <motion.div
+              key={href}
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ delay: 0.2 * index, duration: 1 }}
+            >
+              <Link href={href} legacyBehavior>
+                <a className={styles.navLink}>{href.slice(1).toUpperCase()}</a>
+              </Link>
+            </motion.div>
+          ))}
         </nav>
       </header>
-      <div className={styles['main-content']}>
+      <motion.div
+        className={styles['main-content']}
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 1.5 }}
+      >
         <h1>Bem-vindo à Emell</h1>
         <h3>Terraplenagem de alta qualidade</h3>
         <h3>Seu parceiro de confiança em desenvolvimento de terrenos</h3>
-      </div>
+      </motion.div>
     </div>
   );
 }
