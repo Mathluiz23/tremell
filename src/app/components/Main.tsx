b350e7e3bbb6ca08657f05cdf01c9699f5291f45
Mathluiz23 <matheus_luizs@hotmail.com>
2025-01-24 21:58:41 -0300

    :recycle: refactor: adjusting id's to section page

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -28,7 +28,7 @@ export default function Main() {
           />
         </motion.div>
         <nav className={styles.nav}>
-          {['#sobre', '#localização', '#contato'].map((href, index) => (
+          {['#sobre', '#serviços', '#contato'].map((href, index) => (
             <motion.div
               key={href}
               initial={{ opacity: 0, y: -20 }}
